ffd6ca1a19e3785016e27f1a0d8f5bb8d90d8583
Menard Ysrael Cruz <ysrael214@gmail.com>
2020-03-29 07:59:33 +0800

    added heroku settings

--- import/settings.py
+++ import/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -163,3 +164,5 @@ GOOGLE_API_SCOPES = [
     'https://www.googleapis.com/auth/drive.appdata',
     'https://www.googleapis.com/auth/drive.metadata.readonly'
 ]
+
+django_heroku.settings(locals())
